9645f8859ff74b726e7fb9712055d9b55c90b124
Andrés Villarroel Romero <avillarroelrom@gmail.com>
2024-07-14 17:08:05 -0400

    second_Commit_Test_GET

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,9 @@
 const request = require("supertest");
-const server = require("../index");
+const app = require("../index.js"); // Asegúrate de que la ruta sea correcta
 
 describe("Operaciones CRUD de cafes", () => {
-
+  it("Should respond with a 200 status", async () => {
+    const response = await request(app).get("/cafes").send();
+    expect(response.status).toBe(200);
+  });
 });
